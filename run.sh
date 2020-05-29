e776244e9fa795b24bfb5d6d9110bcfff652eea4
Roman Stark <mail@romanstark.de>
2020-05-29 09:55:44 +0200

    Fixing

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # GET ALL USER INPUT
-tput setaf 4; echo 'Welcome to nodej nginx server on Ubuntu 18.04 install bash script!'; sleep 2;
+tput setaf 4; echo "Welcome to nodej nginx server on Ubuntu 18.04 install bash script!"; sleep 2;
 tput setaf 2;
 read -n 1 -p "Install the server compontents? Usually needed only on fresh system. (y/n) " INSTALL;
 echo
@@ -14,7 +14,6 @@ read PORT
 echo "Where should the app be placed? E.g. /var/www/html or $(pwd)"
 read PATH
 
-
 # If blank server install software components
 if [ "$INSTALL" == "y" ] ;then
   tput setaf 2;
@@ -22,7 +21,7 @@ if [ "$INSTALL" == "y" ] ;then
   echo
   echo
   tput sgr0;
-	echo 'Installing...'; sleep 1;
+	echo "Installing..."; sleep 1;
 	sudo add-apt-repository -y ppa:certbot/certbot
   if [ "$MONGO" == "y" ] ;then
 	  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
@@ -39,22 +38,23 @@ if [ "$INSTALL" == "y" ] ;then
   fi
 	sudo mv /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-available/nginx.conf.backup
 	sudo cp "$(pwd)"/nginx.conf /etc/nginx/
+  sudo unlink /etc/nginx/sites-enabled/default
 	sudo systemctl reload nginx
-	tput setaf 2; echo 'Downloading ssl_dhparam from mozilla.org...'; sleep 1;
+	tput setaf 2; echo "Downloading ssl_dhparam from mozilla.org..."; sleep 1;
 	tput sgr0
 	sudo curl https://ssl-config.mozilla.org/ffdhe2048.txt > /etc/ssl/ffdhe4096.pem
-	tput setaf 2; echo 'Installing PM2 globally...'; sleep 1;
+	tput setaf 2; echo "Installing PM2 globally..."; sleep 1;
 	tput sgr0
   if [ "$MONGO" != "y" ] ;then
 	    npm install -g mongodb-runner --no-optional --no-shrinkwrap
   fi
   npm install -g pm2@latest --no-optional --no-shrinkwrap
-	tput setaf 4; echo 'Installation of packages complete.'; sleep 3;
+	tput setaf 4; echo "Installation of packages complete."; sleep 3;
 	tput sgr0
 fi
 
 # Create the hellow world app and start it
-tput setaf 2; echo 'Configuring the nodejs hello world app.'; sleep 1;
+tput setaf 2; echo "Configuring the nodejs hello world app."; sleep 1;
 echo
 tput sgr0
 mkdir "$PATH/$DOMAIN"
@@ -63,7 +63,7 @@ sed -i -e "s/PORT/$PORT/" "$PATH/$DOMAIN/app.js"
 pm2 start --name "$DOMAIN" "$PATH/$DOMAIN/app.js"
 
 # Setup the domain
-tput setaf 2; echo 'Configuring your domain "$DOMAIN".'; sleep 2;
+tput setaf 2; echo "Configuring your domain $DOMAIN."; sleep 2;
 echo
 tput sgr0
 sudo cp "$(pwd)"/nginx-virtual.conf /etc/nginx/sites-available/"./$DOMAIN.conf"
